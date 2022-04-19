7445f54481cf961aeefa98bcda1f339da36cc6aa
IslombekAdulakhatov <36397876+IslombekAdulakhatov@users.noreply.github.com>
2022-04-18 18:49:08 -0700

    Dicee App iOS

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Islombek Abdulakhatov on 16/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
